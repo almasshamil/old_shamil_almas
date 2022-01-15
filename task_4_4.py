da1300281b602d2da4ec3a3a0fcaac12641bb611
almas_shamil <shamilalmas@gmail.com>
2022-01-15 15:23:09 +0500

    task_3

---
+++ task_4_4.py
@@ -0,0 +1,13 @@
+import utils
+print(utils.currency_rates("USD"))
+print(utils.currency_rates("KZT"))
+print(utils.currency_rates("EUR"))
+
+"""
+/Users/almasshamil/PycharmProjects/shamil_almas/venv/bin/python /Users/almasshamil/PycharmProjects/shamil_almas/task_4_4.py
+(75.7668, datetime.date(2022, 1, 15))
+(17.3677, datetime.date(2022, 1, 15))
+(86.8894, datetime.date(2022, 1, 15))
+
+Process finished with exit code 0
+"""
